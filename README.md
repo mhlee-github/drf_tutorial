f41764d9ba2e5d65104987e1f30abea4d58609f6
Myeonghyeon Lee <myeonghyeon.lee@gmail.com>
2018-03-27 16:36:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# README #
+
+This README would normally document whatever steps are necessary to get your application up and running.
+
+### What is this repository for? ###
+
+* drf tutorial 소스코드 보관
+* http://www.django-rest-framework.org/tutorial/1-serialization/
+
+### How do I get set up? ###
+
+```
+$ pip install -r requirements.txt
+
+$ python manage.py migrate
+
+$ python manage.py runserver
+
+```
---
+++ snippets/urls.py
@@ -0,0 +1,27 @@
+# from django.conf.urls import url
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from snippets import views
+#
+# urlpatterns = [
+#     url(r'^snippets/$', views.SnippetList.as_view()),
+#     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+#     url(r'^users/$', views.UserList.as_view()),
+#     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+#     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
+# ]
+#
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
